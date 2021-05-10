b9cb9da98817243de63d391531fee5e242583577
Alexandro <neurocoso@gmail.com>
2021-05-11 01:22:44 +0200

    added first classes

---
+++ src/main/java/com/example/fabrickrc/FabrickrcApplication.java
@@ -0,0 +1,13 @@
+package com.example.fabrickrc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FabrickrcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FabrickrcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/fabrickrc/FabrickrcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fabrickrc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FabrickrcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
